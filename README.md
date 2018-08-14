e928e64461ad392d7b67d40296416af4b65a1b20
akash <mil10akash@gmail.com>
2018-08-14 10:46:45 +0530

    intial files

---
+++ README.md
@@ -0,0 +1,10 @@
+# INVENTOSARUS
+
+
+Paths
+- / HOME
+- /events Department list
+- /events/:dept Department event list
+- /events/:dept/:event Event Deails
+- /about-us 
+- /organizers
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        helelo
+      </div>
+    );
+  }
+}
+
+export default App;
