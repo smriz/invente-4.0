f8397a32f3f2f245d6fd29cfec22e10d3b001c33
akash <mil10akash@gmail.com>
2018-08-19 00:05:36 +0530

    Home+navi

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,15 +1,30 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 class Navigator extends React.Component{
 		constructor(props){
 			super(props);
+			this.routeMap = {
+				'Events' :'/events',
+				'Organizers':'/organizers',
+				'About Us':'/about-us',
+				'Hospitality':'/hospitality'
+			}
+			this.state = {
+				title:props.title
+			}
 		}
 		render(){
 			return (
 				<div className='navigator'>
-					<div>Invente_logo</div>
-					<div>
-						links
+					<div className='main'>
+						{this.state.title}
+					</div>
+					<div className='links'>
+						{
+							Object.keys(this.routeMap).map(key => 
+  								<Link to={this.routeMap[key]}>{key}</Link>
+  							)
+						}
 					</div>		
 				</div>
 			);
