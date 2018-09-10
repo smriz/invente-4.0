e7d95fd84991f4bbe89800f301d68393d79aa204
akash <mil10akash@gmail.com>
2018-09-10 16:57:07 +0530

    event background

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -6,7 +6,7 @@ class Aboutus extends React.Component{
 
     render(){
         return(
-            <div>
+            <div className='whatsapp'>
             <Helmet>
               <title>About Us | Invente 3.0</title>
             </Helmet>
